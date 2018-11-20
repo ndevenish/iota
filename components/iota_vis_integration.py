36a3871e975831d25cd14449b268e85403781ff8
Artem Lyubimov <lyubimov@stanford.edu>
2018-11-20 09:11:49 -0800

    wxPython 4 compatibility
    - Created a iota_ui_base module for base Frames, Windows, etc. (idea is to use
    base classes for a wxPython version check)
    - Separate iota_plotter module (for plots in separate window instead of PyPlot,
    which doesn't work well with wxPython 4 in Matplotlib 2.0; will later become a
    module with base plot classes)
    - Clunky so far, will update with a more general approach later

--- components/iota_vis_integration.py
+++ components/iota_vis_integration.py
@@ -9,7 +9,6 @@ Description : Creates a PNG file visualizing integration results.
 
 from libtbx import easy_pickle as ep
 import numpy as np
-import matplotlib.pyplot as plt
 import matplotlib as mpl
 
 
@@ -55,6 +54,8 @@ def make_png(
     mosaicity = int_d["mosaicity"]
 
     # Create the figure
+    import matplotlib.pyplot as plt
+
     fig = plt.figure(figsize=(6, 6))
 
     # 1st set of axes for the image
@@ -144,6 +145,8 @@ def cv_png(image_pickle, integration_pickle, file_name=None, res=600, show_spots
     mosaicity = int_d["mosaicity"]
 
     # Create the figure
+    import matplotlib.pyplot as plt
+
     fig = plt.figure(figsize=(6, 6))
 
     # 1st set of axes for the image
